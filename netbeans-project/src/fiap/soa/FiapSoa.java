987287ab19ce499ad630eefebff380a4c382eb57
ricardodestro <ricardo.destro@gmail.com>
2016-09-08 00:42:12 -0300

    versao final

---
+++ netbeans-project/src/fiap/soa/FiapSoa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fiap.soa;
+
+/**
+ *
+ * @author destro
+ */
+public class FiapSoa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.setVisible(true);
+    }
+
+}
